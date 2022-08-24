1960bcd6ffbfa6b6868522b2674a1370e7234bbb
Zhengyuan Su <su-zy21@mails.tsinghua.edu.cn>
2022-08-24 17:29:31 +0800

    Update Urls for connection.

--- tsmsp-frontend-template-master/src/Globals/GlobalVariables.tsx
+++ tsmsp-frontend-template-master/src/Globals/GlobalVariables.tsx
@@ -1,1 +1,5 @@
 export const APIUrl : string = "http://localhost:6070/api"
+
+//For Web: "http://localhost:6070/api"
+//For Android Emulator: "http://10.0.2.2:6070/api"
+//For Cellphone: "http://$Computer_IP:6070/api"
