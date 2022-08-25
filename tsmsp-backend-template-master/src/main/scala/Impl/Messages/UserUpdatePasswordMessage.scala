1eeb23a47f6b55112a2f3094be795e9ef0bc4d4c
happy-potatoes <2003_jim@sina.com>
2022-08-25 10:41:43 +0800

    now backend does not contain compile error...

--- tsmsp-backend-template-master/src/main/scala/Impl/Messages/UserUpdatePasswordMessage.scala
+++ tsmsp-backend-template-master/src/main/scala/Impl/Messages/UserUpdatePasswordMessage.scala
@@ -1,5 +1,4 @@
 package Impl.Messages
-
 import Exceptions.UserNameAlreadyExistsException
 import Impl.{STATUS_OK, TSMSPReply}
 import Tables.{UserTable, UserTokenTable}
@@ -12,7 +11,8 @@ case class UserUpdatePasswordMessage(userToken : String, password : String) exte
   override def reaction(now: DateTime): Try[TSMSPReply] = Try {
     // TODO: update password properly
     val userName = UserTokenTable.checkUserName(userToken).get
-    DBUtils.exec(UserTable.updatePassword(userName, password).get)
-    TSMSPReply(STATUS_OK,  UserTokenTable.checkToken(userName).get)
+    TSMSPReply(STATUS_OK, "I received this request.")
+    //DBUtils.exec(UserTable.updatePassword(userName, password).get)
+    //TSMSPReply(STATUS_OK,  UserTokenTable.checkToken(userName).get)
   }
 }
