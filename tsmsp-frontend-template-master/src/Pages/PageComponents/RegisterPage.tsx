115ed620b83609f8291aaae7ae826fdf3a711796
Zhengyuan Su <su-zy21@mails.tsinghua.edu.cn>
2022-08-29 12:13:29 +0800

    Add register info check.

--- tsmsp-frontend-template-master/src/Pages/PageComponents/RegisterPage.tsx
+++ tsmsp-frontend-template-master/src/Pages/PageComponents/RegisterPage.tsx
@@ -7,28 +7,35 @@ import {ButtonTemplate, ButtonToSendMessage} from "../../Utils/PageUtils/PageBut
 import {TSMSPReply} from "../../Impl/TSMSPReply";
 import {PageContainerTemplate} from "../../Utils/PageUtils/PageContainerUtil";
 import {TextInputTemplate} from "../../Utils/PageUtils/TextInputUtil";
+import {checkIdentityNumber} from "../../Utils/FormatUtils/IdentityNumberUtil";
+import {checkPassword} from "../../Utils/FormatUtils/PasswordUtil";
+import {checkRealName} from "../../Utils/FormatUtils/RealNameUtil";
 
 const registerStore= create(() => ({
-    userName:"",
-    password:"",
     realName:"",
+    password:"",
+    identityNumber:"",
 }))
 
-const setUserName= (userName:string) => registerStore.setState({ userName })
+const setUserName= (realName:string) => registerStore.setState({ realName })
 const setPassword= (password:string) => registerStore.setState({ password })
-const setRealName= (realName:string) => registerStore.setState({ realName })
-const clearRegisterInfo= ()=> registerStore.setState(({userName: "", password: "", realName: ""}))
+const setRealName= (identityNumber:string) => registerStore.setState({ identityNumber })
+const clearRegisterInfo= ()=> registerStore.setState(({realName: "", password: "", identityNumber: ""}))
 
 export function RegisterPage({ navigation }: any){
-    const {userName,password, realName}=registerStore()
+    const {realName, password, identityNumber}=registerStore()
     return <PageContainerTemplate>
-        <TextInputTemplate placeholder={"真实姓名"} value={userName} onChangeText={(newText: string)=>setUserName(newText)}/>
+        <TextInputTemplate placeholder={"真实姓名"} value={realName} onChangeText={(newText: string)=>setUserName(newText)}/>
         <TextInputTemplate placeholder={"密码"}  value={password} onChangeText={(newText: string)=>setPassword(newText)} secureTextEntry={true}/>
-        <TextInputTemplate placeholder={"身份证号"}  value={realName} onChangeText={(newText: string)=>setRealName(newText)}/>
+        <TextInputTemplate placeholder={"身份证号"} value={identityNumber} onChangeText={(newText: string)=>setRealName(newText)}/>
 
         {/*console.log("试图使用用户名"+userName+",密码"+password + ",真实姓名"+realName + "注册！")*/}
         <ButtonToSendMessage
-            toSendMessage ={new UserRegisterMessage(userName, password, realName)}
+            checkBeforeSendMessage = {()=>(checkRealName(realName) && checkPassword(password) && checkIdentityNumber(identityNumber))}
+            checkElse = {()=>{
+                alert("姓名、密码（至少六位）或身份证号不符要求！")
+            }}
+            toSendMessage ={new UserRegisterMessage(realName, password, identityNumber)}
             text = '注册'
             ifSuccess = {(replyJson: TSMSPReply)=>{
                 setUserToken(replyJson.message);
