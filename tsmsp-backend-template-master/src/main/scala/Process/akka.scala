2ac3c684f5f9b43ff2fb90a40f4d7a57415ebba9
happy-potatoes <2003_jim@sina.com>
2022-08-30 14:09:35 +0800

    client server with failure case

--- tsmsp-backend-template-master/src/main/scala/Process/akka.scala
+++ tsmsp-backend-template-master/src/main/scala/Process/akka.scala
@@ -9,6 +9,7 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.util.Timeout
 
 import scala.concurrent.duration.DurationInt
+import scala.util.{Failure, Success}
 
 object Serv {
   case class Request(query: String, replyTo: ActorRef[Response])
@@ -16,11 +17,40 @@ object Serv {
   def apply(): Behaviors.Receive[Request] =
     Behaviors.receiveMessage[Request] {
       case Request(query, replyTo) =>
-        replyTo ! Response("Your response")
+        replyTo ! Response(s"$query: Your response")
         Behaviors.same
     }
 }
 
+object Client {
+  sealed trait Command
+  private case class AdaptedResponse(message: String) extends Command
+  def apply(server: ActorRef[Request]): Behavior[Command] = {
+    Behaviors.setup[Command] {ctx =>
+      implicit val timeout: Timeout = 3.seconds
+      ctx.ask(server, ref => Serv.Request("request1", ref)) {
+        case Success(Serv.Response(message)) => AdaptedResponse(message)
+        case Failure(_) => AdaptedResponse("request failed")
+      }
+      Behaviors.receiveMessage {
+        case AdaptedResponse(message) => ctx.log.info(s"response received, message: $message")
+          Behaviors.stopped
+      }
+    }
+  }
+}
+
+object St {
+  def apply(): Behavior[NotUsed] = Behaviors.setup {ctx =>
+    val server = ctx.spawn(Serv(), "server")
+    val client = ctx.spawn(Client(server), "client")
+    Behaviors.empty
+  }
+  def main(args: Array[String]): Unit = {
+    ActorSystem(St(), "system")
+  }
+}
+/*
 case class Client() {
   def init(server: ActorRef[Request]): Behavior[Response] = Behaviors.setup {c =>
     server ! Request("request 1", c.self)
@@ -41,3 +71,4 @@ object StartUp {
     ActorSystem(StartUp(), "client")
   }
 }
+ */
