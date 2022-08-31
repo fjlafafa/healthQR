e63c1a7314c4d615b399d8dde6fabed27abf95ea
Zhengyuan Su <su-zy21@mails.tsinghua.edu.cn>
2022-08-31 20:22:11 +0800

    merge

--- tsmsp-frontend-template-master/src/Globals/GlobalVariables.tsx
+++ tsmsp-frontend-template-master/src/Globals/GlobalVariables.tsx
@@ -2,11 +2,7 @@ export const AllowAdmin : boolean = true
 //When set to false, will close all admin function
 
 
-<<<<<<< HEAD
-export const APIUrl : string = "http://183.173.176.83:6070/api"
-=======
 export const APIUrl : string = "http://localhost:6070/api"
->>>>>>> f21046113c29240ceefb8bd4491e5c5ebfd16e11
 
 //For Web: 'http://localhost:6070/api'
 //For Android Emulator: 'http://10.0.2.2:6070/api'
