d8491ce11a02e9df4e88852e05d39b17a1628676
Zhengyuan Su <su-zy21@mails.tsinghua.edu.cn>
2022-09-03 23:26:20 +0800

    Fix time incorrectness.

--- tsmsp-backend-template-master/src/main/scala/Impl/Messages/UserMessages/UserGetTraceMessage.scala
+++ tsmsp-backend-template-master/src/main/scala/Impl/Messages/UserMessages/UserGetTraceMessage.scala
@@ -11,9 +11,9 @@ import org.joda.time.DateTime
 
 import scala.util.Try
 
-case class UserGetTraceMessage(userToken: String, startTime: DateTime, endTime: DateTime) extends TSMSPMessage {
+case class UserGetTraceMessage(userToken: String, startTime: Long, endTime: Long) extends TSMSPMessage {
   override def reaction(now: DateTime): Try[TSMSPReply] = Try {
     val userId = UserIdentityTable.checkUserId(userToken).get
-    UserInfoMSMessages.MSUserGetTraceMessage(userId, startTime, endTime).send(GlobalVariables.UserInfoMSIP).get
+    UserInfoMSMessages.MSUserGetTraceMessage(userId, new DateTime(startTime), new DateTime(endTime)).send(GlobalVariables.UserInfoMSIP).get
   }
 }
