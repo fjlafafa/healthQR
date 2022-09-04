a024db3b68b14c9a7e7de81c0b9df71a2fc90e23
fjlafafa <wmc21@mails.tsinghua.edu.cn>
2022-09-04 11:37:30 +0800

    NA page

--- tsmsp-frontend-template-master/src/Impl/Messages/ThirdPartyMessages/HospitalUpdateNucleicTestMessage.tsx
+++ tsmsp-frontend-template-master/src/Impl/Messages/ThirdPartyMessages/HospitalUpdateNucleicTestMessage.tsx
@@ -2,7 +2,8 @@ import {TSMSPMessage} from '../TSMSPMessage'
 
 export class HospitalUpdateNucleicTestMessage extends TSMSPMessage {
     identityNumber : string
-    constructor(identityNumber : string) {
+
+    constructor(identityNumber: string) {
         super()
         this.identityNumber = identityNumber
     }
--- tsmsp-frontend-template-master/src/Pages/PageComponents/ThirdPartyPages/NucleicAcidPage.tsx
+++ tsmsp-frontend-template-master/src/Pages/PageComponents/ThirdPartyPages/NucleicAcidPage.tsx
@@ -1,10 +1,30 @@
 import {ScreenTemplate} from "../../../Utils/PageUtils/PageContainerUtil";
+import {HospitalUpdateNucleicTestMessage} from "Messages/ThirdPartyMessages/HospitalUpdateNucleicTestMessage";
+import create from 'zustand'
+import {ButtonToSendMessage} from "Utils/PageUtils/ButtonUtil";
+import React from "react";
+import {TextInputTemplate} from "Utils/PageUtils/TextInputUtil";
+import {checkIdentityNumber} from "Utils/FormatUtils/IdentityNumberUtil";
 
 export function NucleicAcidPage({navigation}:any){
 
+const IDStore = create(()=> ({
+        identity: ''
+}))
+const setIdentity = (identity: string) => IDStore.setState({identity})
+const goBack=()=>navigation.navigate('ThirdParty.Overview')
+const {identity} = IDStore()
+//@ts-ignore
+return <ScreenTemplate goBack={goBack}>
 
-    const goBack=()=>navigation.navigate('ThirdParty.Overview')
+    <TextInputTemplate label='受检人身份证号' value={identity} onChangeText={(newText: string)=>setIdentity(newText)}/>
 
-    return <ScreenTemplate goBack={goBack}>
-    </ScreenTemplate>
+    <ButtonToSendMessage
+        checkBeforeSendMessage = {()=>(checkIdentityNumber(identity))}
+        checkElse = {'请重新检查身份证号是否填写正确'}
+        toSendMessage ={new HospitalUpdateNucleicTestMessage(identity)}
+        text = '上传'
+        ifSuccess = {IDStore.setState({identity:""})}
+    />
+</ScreenTemplate>
 }
