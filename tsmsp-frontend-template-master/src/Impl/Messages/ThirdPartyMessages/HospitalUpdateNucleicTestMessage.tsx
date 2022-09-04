ea2af988f545fd135f616a19cc1976e9d4380b7d
fjlafafa <wmc21@mails.tsinghua.edu.cn>
2022-09-04 16:25:02 +0800

    remove some redundancy

--- tsmsp-frontend-template-master/src/Impl/Messages/ThirdPartyMessages/HospitalUpdateNucleicTestMessage.tsx
+++ tsmsp-frontend-template-master/src/Impl/Messages/ThirdPartyMessages/HospitalUpdateNucleicTestMessage.tsx
@@ -2,9 +2,12 @@ import {TSMSPMessage} from '../TSMSPMessage'
 
 export class HospitalUpdateNucleicTestMessage extends TSMSPMessage {
     identityNumber : string
+    token : string
 
-    constructor(identityNumber: string) {
+
+    constructor(identityNumber: string, token: string) {
         super()
         this.identityNumber = identityNumber
+        this.token = token
     }
 }
--- tsmsp-frontend-template-master/src/Utils/Constants.tsx
+++
@@ -1,1 +0,0 @@
-export const ONEDAY = 24*60*60*1000
