7150c5578a2faff0b6b7edbc36240cce84573018
happy-potatoes <2003_jim@sina.com>
2022-09-07 20:55:59 +0800

    red code after positive result

--- tsmsp-backend-template-master/src/main/scala/Impl/Messages/ThirdPartyMessages/HospitalUploadPositiveNucleicTestResultByTokenMessage.scala
+++ tsmsp-backend-template-master/src/main/scala/Impl/Messages/ThirdPartyMessages/HospitalUploadPositiveNucleicTestResultByTokenMessage.scala
@@ -5,9 +5,10 @@ import Globals.GlobalVariables
 import Impl.Messages.MSCommunicationMessages.UserInfoMSMessages.MSHospitalDiffusionMessage
 import Impl.Messages.TSMSPMessage
 import Impl.TSMSPReply
+import Tables.PermissionRoleTable.checkPermission
 import Tables.UserIdentityTable
 import Types.UserMeta._
-import Utils.DBUtils
+import Utils.{DBUtils, IOUtils}
 import Utils.HTTPUtils.sender
 import org.joda.time.DateTime
 
@@ -15,14 +16,15 @@ import scala.util.Try
 
 case class HospitalUploadPositiveNucleicTestResultByTokenMessage(userToken: Token, clientToken: Token) extends TSMSPMessage {
   override def reaction(now: DateTime): Try[TSMSPReply] = Try {
-    val permission = UserIdentityTable.getRoleFromToken(userToken).get
-    if (permission != NucleicTestResultReporter) {
+    val role = UserIdentityTable.getRoleFromToken(userToken).get
+    if (!checkPermission(role, UpdateNucleicTest)) {
       throw PermissionDeniedException()
     }
 
     val diagnosedUserId: List[UserId] = List(DBUtils.exec(
       UserIdentityTable.checkUserIdByToken(clientToken)
     ).getOrElse(throw TokenNotExistsException()))
+    println(IOUtils.serialize(diagnosedUserId))
     MSHospitalDiffusionMessage(diagnosedUserId).send(GlobalVariables.UserInfoMSIP).get
   }
 }
